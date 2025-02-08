03947c7620de9d2e087435c33fdbc58c5d6e190a
Shinya Okazaki <negichou@gmail.com>
2025-02-09 04:58:52 +0900

    [add] AppDelegate, SceneDelegate を導入

---
+++ AbstinenceSupportPackage/Sources/AbstinenceSupportCore/SceneDelegate.swift
@@ -0,0 +1,15 @@
+// Created by okazakishinya on 2025/02/09.
+
+import Foundation
+import UIKit
+
+open class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    public var window: UIWindow?
+
+    public final func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {}
+}
--- Projects/AbstinenceSupport/AbstinenceSupport/AbstinenceSupportApp.swift
+++ Projects/AbstinenceSupport/AbstinenceSupport/AbstinenceSupportApp.swift
@@ -1,9 +1,13 @@
 // Created by okazakishinya on 2025/02/09.
 
 import SwiftUI
+import AbstinenceSupportCore
 
 @main
 struct AbstinenceSupportApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
