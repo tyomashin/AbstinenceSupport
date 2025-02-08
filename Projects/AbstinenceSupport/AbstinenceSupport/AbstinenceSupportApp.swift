b83a9d6ae8d2790b501423d3e07a581412dfd80c
Shinya Okazaki <negichou@gmail.com>
2025-02-09 04:18:34 +0900

    [add] プロジェクトを新規作成

---
+++ Projects/AbstinenceSupport/AbstinenceSupport/AbstinenceSupportApp.swift
@@ -0,0 +1,12 @@
+// Created by okazakishinya on 2025/02/09.
+
+import SwiftUI
+
+@main
+struct AbstinenceSupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projects/AbstinenceSupport/AbstinenceSupport/ContentView.swift
@@ -0,0 +1,19 @@
+// Created by okazakishinya on 2025/02/09.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
