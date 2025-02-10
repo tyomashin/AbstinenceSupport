cb570bd1a1a612540ff0a5cdd039fb0f466646cf
Shinya Okazaki <negichou@gmail.com>
2025-02-10 23:22:41 +0900

    [add] UIコンポーネントとして各種ラベルを追加
    ・一括でフォントサイズや文字色などを変更できるように、TextをカスタムViewでラップ

---
+++ AbstinenceSupportPackage/Sources/Presentation/Components/Label/BodyLabel.swift
@@ -0,0 +1,27 @@
+// Created by okazakishinya on 2025/02/10.
+
+import SwiftUI
+import Common
+
+public struct BodyLabel: View {
+
+    let text: LocalizedString
+    let colorAssets: ColorAssets
+
+    public init(_ text: LocalizedString, colorAssets: ColorAssets = ColorAssets.subText) {
+        self.text = text
+        self.colorAssets = colorAssets
+    }
+
+    public var body: some View {
+        Text(text.localizedString)
+            .foregroundStyle(colorAssets.color)
+            .font(.body)
+            .fontWeight(.regular)
+            .frame(maxWidth: .infinity)
+    }
+}
+
+#Preview {
+    BodyLabel(.onboarding1Title)
+}
