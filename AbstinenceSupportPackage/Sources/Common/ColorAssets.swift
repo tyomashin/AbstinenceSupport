91363e8413e5acaec1297e0d40e947923e3ad1b9
Shinya Okazaki <negichou@gmail.com>
2025-02-10 23:20:35 +0900

    [add] 色定義を追加

--- AbstinenceSupportPackage/Sources/Common/ColorAssets.swift
+++ AbstinenceSupportPackage/Sources/Common/ColorAssets.swift
@@ -9,5 +9,53 @@ import SwiftUI
 ///     Xcode によって Asset Symbol は自動生成されるがアクセスレベルが public ではないため他モジュールからは直接参照できない
 ///     https://developer.apple.com/forums/thread/756954
 public enum ColorAssets {
-    public static let baseText = Color.baseText
+    case baseBland
+    case subBland
+    case deepBland
+    case baseAccent
+    case subAccent
+    case deepAccent
+    case baseBackground
+    case subBackground
+    case baseText
+    case subText
+    case baseWhite
+    case placeholder
+    case shadow
+    case baseDisabled
+}
+
+extension ColorAssets {
+    public var color: Color {
+        switch self {
+        case .baseBland:
+            Color.baseBland
+        case .subBland:
+            Color.subBland
+        case .deepBland:
+            Color.deepBland
+        case .baseAccent:
+            Color.baseAccent
+        case .subAccent:
+            Color.subAccent
+        case .deepAccent:
+            Color.deepAccent
+        case .baseBackground:
+            Color.baseBackground
+        case .subBackground:
+            Color.subBackground
+        case .baseText:
+            Color.baseText
+        case .subText:
+            Color.subText
+        case .baseWhite:
+            Color.baseWhite
+        case .placeholder:
+            Color.placeholder
+        case .shadow:
+            Color.shadow
+        case .baseDisabled:
+            Color.baseDisabled
+        }
+    }
 }
