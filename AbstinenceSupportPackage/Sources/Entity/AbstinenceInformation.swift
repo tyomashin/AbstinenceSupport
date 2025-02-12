9efb99eeef41490579a1f944b10ff1ec6593a7da
Shinya Okazaki <negichou@gmail.com>
2025-02-12 21:15:04 +0900

    [update] 禁欲情報Entityに計算型プロパティを追加

--- AbstinenceSupportPackage/Sources/Entity/AbstinenceInformation.swift
+++ AbstinenceSupportPackage/Sources/Entity/AbstinenceInformation.swift
@@ -1,6 +1,7 @@
 // Created by okazakishinya on 2025/02/12.
 
 import Foundation
+import Common
 
 /// 禁欲情報
 public struct AbstinenceInformation: Codable, Sendable {
@@ -21,4 +22,13 @@ public struct AbstinenceInformation: Codable, Sendable {
     public var progressStatus: AbstinenceProgressStatus
     /// 禁欲開始日時
     public let startDate: Date
+    /// 進捗率(0.0〜1.0)
+    public var rateOfProgress: Float {
+        guard normalizedTargetDays > 0 else { return 1 }
+        return min(Float(elapsedDays) / Float(normalizedTargetDays), 1)
+    }
+    /// 開始日時からの経過日数
+    public var elapsedDays: Int {
+        max(0, DateUtils.daysBetween(from: startDate, to: Date()))
+    }
 }
