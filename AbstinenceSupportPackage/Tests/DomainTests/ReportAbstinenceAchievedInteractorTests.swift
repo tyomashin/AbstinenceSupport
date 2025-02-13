0724085d209ba66792f27e46619546c69dc6b997
Shinya Okazaki <negichou@gmail.com>
2025-02-14 01:34:38 +0900

    [add] ReportAbstinenceAchievedInteractor のテストを追加

---
+++ AbstinenceSupportPackage/Tests/DomainTests/ReportAbstinenceAchievedInteractorTests.swift
@@ -0,0 +1,35 @@
+// Created by okazakishinya on 2025/02/14.
+
+import Foundation
+import Testing
+import Dependencies
+@testable import Domain
+@testable import Entity
+@testable import Common
+@testable import TestHelper
+
+struct ReportAbstinenceAchievedInteractorTests {
+
+    var interactor: ReportAbstinenceAchievedInteractor!
+    let keyChainHelperStub: KeyChainHelperStub
+    var testEntity = AbstinenceInformation(title: "sample", detail: nil, targetDays: 0, scheduledReportDate: Date(), penalties: .free, progressStatus: .inProgress, startDate: Date(), currentReportedDate: nil)
+    
+    init() {
+        self.keyChainHelperStub = KeyChainHelperStub()
+        interactor = withDependencies {
+            $0.keyChainHelper = keyChainHelperStub
+        } operation: {
+            ReportAbstinenceAchievedInteractor()
+        }
+    }
+    
+    @Test("報告日時が正常に保存されていることを確認", arguments: [
+        Date(),
+        DateUtils.makeDate(year: 2024, month: 1, day: 1)!,
+        DateUtils.makeDate(year: 2025, month: 12, day: 31)!,
+    ]) func reportAbstinenceAchieved(reportDate: Date) async throws {
+        let result = await interactor.execute(with: testEntity, reportDate: reportDate)
+        #expect(result.currentReportedDate == reportDate)
+        #expect(keyChainHelperStub.abstinenceInformation == result)
+    }
+}
