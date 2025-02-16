eabda72e81bad18e7e80cdbfe9e64231ba343fa8
Shinya Okazaki <negichou@gmail.com>
2025-02-17 03:55:42 +0900

    [update] ペナルティ情報のEntityを追加

--- AbstinenceSupportPackage/Sources/Entity/Penalties.swift
+++ AbstinenceSupportPackage/Sources/Entity/Penalties.swift
@@ -1,6 +1,7 @@
 // Created by okazakishinya on 2025/02/12.
 
 import Foundation
+import Common
 
 /// ペナルティ種別
 public enum Penalties: Codable, Sendable, Equatable {
@@ -8,4 +9,25 @@ public enum Penalties: Codable, Sendable, Equatable {
     case free
     /// 有料ペナルティ
     case product(productID: String)
+}
+
+/// ペナルティ情報
+public struct PenaltyInfo: Sendable, Equatable {
+    public let title: String
+    public let detail: String
+    public let penaltyCategory: Penalties
+
+    public init(title: String, detail: String, penaltyCategory: Penalties) {
+        self.title = title
+        self.detail = detail
+        self.penaltyCategory = penaltyCategory
+    }
+
+    public static func freePenaltyInfo() -> Self {
+        .init(
+            title: LocalizedString.penaltyFreeTitle.localizedString,
+            detail: LocalizedString.penaltyFreeDetail.localizedString,
+            penaltyCategory: .free
+        )
+    }
 }
